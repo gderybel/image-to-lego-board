ac7257fd24570a9180df11467bc30b50d3a449fb
gderybel <guillaume.derybel@protonmail.com>
2025-10-17 17:57:56 +0200

    get colors from bricklink

--- BrickLinkConnector.py
+++ BrickLinkConnector.py
@@ -1,14 +1,19 @@
 from LegoPiece import LegoPiece
+from LegoColor import LegoColor
 import requests
 from bs4 import BeautifulSoup
+import re
+import lru_cache
 
 
 class BrickLinkConnector:
-    base_url = "https://www.bricklink.com/v2/catalog/catalogitem.page?P="
+    buy_url = "https://www.bricklink.com/v2/catalog/catalogitem.page?P="
+    color_url = "https://v2.bricklink.com/en-fr/catalog/color-guide"
 
+    @staticmethod
     def get_piece_price(piece: LegoPiece) -> float:
 
-        url = f"{BrickLinkConnector.base_url}={piece.id}"
+        url = f"{BrickLinkConnector.buy_url}={piece.id}"
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
 
@@ -21,3 +26,59 @@ class BrickLinkConnector:
             except ValueError:
                 return 0.0
         return 0.0
+
+    @staticmethod
+    @lru_cache(maxsize=255)
+    def get_piece_colors(type: str = "solid") -> list[str]:
+        url = f"{BrickLinkConnector.color_url}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        
+        pattern = re.compile(
+            r"--bl-castor-table-swatch-with-image-background-color:\s*(#[0-9a-fA-F]{3,6})"
+        )
+
+        colors = []
+
+        for tr in soup.find_all("tr"):
+            color_hex = None
+            bricklink_name = None
+            bricklink_id = None
+            lego_name = None
+            lego_id = None
+
+            td_with_style = tr.find("td", attrs={"data-testid": "SwatchWithImage"})
+            if td_with_style and "style" in td_with_style.attrs:
+                match = pattern.search(td_with_style["style"])
+                if match:
+                    color_hex = match.group(1)
+
+            color_info_td = tr.find("td", class_="color-list-wide-viewport_cellListViewColorName__rVQgC")
+            if color_info_td:
+                name_tag = color_info_td.find("p")
+                if name_tag:
+                    bricklink_name = name_tag.get_text(strip=True)
+                span_tag = color_info_td.find("span")
+                if span_tag:
+                    lego_color = span_tag.get_text(strip=True).replace("LEGO Color:", "")
+                    match = re.match(r'^(.*?)\s*-\s*(\d+)$', lego_color)
+                    if match:
+                        lego_name= match.group(1)
+                        lego_id = int(match.group(2))
+
+            tds = tr.find_all("td")
+            if tds:
+                bricklink_id = tds[-1].get_text(strip=True)
+
+            if color_hex and bricklink_name and lego_name and lego_id and bricklink_id:
+                colors.append(
+                    LegoColor(
+                        bricklink_name=bricklink_name,
+                        lego_name=lego_name,
+                        bricklink_id=bricklink_id,
+                        lego_id=lego_id,
+                        hex_code=color_hex
+                    )
+                )
+
+        return colors
--- LegoColor.py
+++ LegoColor.py
@@ -1,14 +1,19 @@
 from functools import lru_cache
 from Color import Color
+from BrickLinkConnector import BrickLinkConnector
 
 
 class LegoColor(Color):
-    def __init__(self, name: str, hex_code: str):
-        self.name = name
+    def __init__(self, bricklink_name: str, lego_name: str, bricklink_id: str, lego_id:str, hex_code: str):
+        self.bricklink_name = bricklink_name
+        self.lego_name = lego_name
+        self.bricklink_id = bricklink_id
+        self.lego_id = lego_id
         super().__init__(hex_code)
 
     @staticmethod
     def get_closest_lego_color(color: Color) -> "LegoColor":
+        lego_colors = BrickLinkConnector.get_piece_colors()
         r1, g1, b1 = color.rgb_code
         best_dist = float("inf")
         best_color = None
@@ -25,106 +30,9 @@ class LegoColor(Color):
     @staticmethod
     @lru_cache(maxsize=256)
     def get_lego_color_by_name(name: str) -> "LegoColor":
+        lego_colors = BrickLinkConnector.get_piece_colors()
         key = name.strip().upper()
         for lego_color in lego_colors:
-            if lego_color.name == key:
+            if lego_color.bricklink_name == key:
                 return lego_color
         raise ValueError(f"No LegoColor named '{name}'")
-
-
-lego_colors = [
-    LegoColor("AQUA", "#BCE5DC"),
-    LegoColor("BLACK", "#212121"),
-    LegoColor("BLUE", "#0057A6"),
-    LegoColor("BRIGHT_GREEN", "#10CB31"),
-    LegoColor("BRIGHT_LIGHT_BLUE", "#BCD1ED"),
-    LegoColor("BRIGHT_LIGHT_ORANGE", "#FFC700"),
-    LegoColor("BRIGHT_LIGHT_YELLOW", "#FFF08C"),
-    LegoColor("BRIGHT_PINK", "#F7BCDA"),
-    LegoColor("BROWN", "#6B3F22"),
-    LegoColor("CORAL", "#FF8172"),
-    LegoColor("DARK_AZURE", "#009FE0"),
-    LegoColor("DARK_BLUE", "#243757"),
-    LegoColor("DARK_BLUISH_GRAY", "#595D60"),
-    LegoColor("DARK_BROWN", "#50372F"),
-    LegoColor("DARK_GRAY", "#6B5A5A"),
-    LegoColor("DARK_GREEN", "#2E5543"),
-    LegoColor("DARK_NOUGAT", "#CE7942"),
-    LegoColor("DARK_ORANGE", "#B35408"),
-    LegoColor("DARK_PINK", "#EF5BB3"),
-    LegoColor("DARK_PURPLE", "#5F2683"),
-    LegoColor("DARK_RED", "#6A0E15"),
-    LegoColor("DARK_TAN", "#B89869"),
-    LegoColor("DARK_TURQUOISE", "#00A29F"),
-    LegoColor("DARK_YELLOW", "#DD982E"),
-    LegoColor("GREEN", "#00923D"),
-    LegoColor("LAVENDER", "#D3BDE3"),
-    LegoColor("LIGHT_AQUA", "#CFEFEA"),
-    LegoColor("LIGHT_BLUISH_GRAY", "#AFB5C7"),
-    LegoColor("LIGHT_BROWN", "#99663E"),
-    LegoColor("LIGHT_GRAY", "#9C9C9C"),
-    LegoColor("LIGHT_GREEN", "#D7EED1"),
-    LegoColor("LIGHT_NOUGAT", "#FECCB0"),
-    LegoColor("LIGHT_ORANGE", "#FFBC36"),
-    LegoColor("LIGHT_SALMON", "#FCC7B7"),
-    LegoColor("LIGHT_TURQUOISE", "#00C5BC"),
-    LegoColor("LIGHT_YELLOW", "#FEE89F"),
-    LegoColor("LIME", "#C4E000"),
-    LegoColor("MAERSK_BLUE", "#7DC1D8"),
-    LegoColor("MAGENTA", "#B72276"),
-    LegoColor("MEDIUM_AZURE", "#6ACEE0"),
-    LegoColor("MEDIUM_BLUE", "#82ADD8"),
-    LegoColor("MEDIUM_LAVENDER", "#C689D9"),
-    LegoColor("MEDIUM_NOUGAT", "#E3A05B"),
-    LegoColor("MEDIUM_ORANGE", "#FFA531"),
-    LegoColor("MEDIUM_VIOLET", "#9391E4"),
-    LegoColor("NEON_YELLOW", "#FFFC00"),
-    LegoColor("NOUGAT", "#FFAF7D"),
-    LegoColor("OLIVE_GREEN", "#ABA953"),
-    LegoColor("ORANGE", "#FF7E14"),
-    LegoColor("PINK", "#F5CDD6"),
-    LegoColor("PURPLE", "#7A238D"),
-    LegoColor("RED", "#B30006"),
-    LegoColor("REDDISH_BROWN", "#82422A"),
-    LegoColor("REDDISH_ORANGE", "#FF5500"),
-    LegoColor("ROSE_PINK", "#F2D3D1"),
-    LegoColor("RUST", "#B24817"),
-    LegoColor("SAND_BLUE", "#8899AB"),
-    LegoColor("SAND_GREEN", "#A2BFA3"),
-    LegoColor("TAN", "#EED9A4"),
-    LegoColor("VERY_LIGHT_BLUISH_GRAY", "#E4E8E8"),
-    LegoColor("VERY_LIGHT_GRAY", "#E8E8E8"),
-    LegoColor("VERY_LIGHT_ORANGE", "#FFDCA4"),
-    LegoColor("WHITE", "#FFFFFF"),
-    LegoColor("YELLOW", "#FFE001"),
-    LegoColor("YELLOWISH_GREEN", "#E7F2A7"),
-    LegoColor("TRANS_AQUA", "#B7C8BF"),
-    LegoColor("TRANS_BLACK", "#777777"),
-    LegoColor("TRANS_BRIGHT_GREEN", "#10CB31"),
-    LegoColor("TRANS_BROWN", "#939484"),
-    LegoColor("TRANS_CLEAR", "#EEEEEE"),
-    LegoColor("TRANS_DARK_BLUE", "#00296B"),
-    LegoColor("TRANS_GREEN", "#217625"),
-    LegoColor("TRANS_LIGHT_BLUE", "#68BCC5"),
-    LegoColor("TRANS_LIGHT_BRIGHT_GREEN", "#71EB54"),
-    LegoColor("TRANS_LIGHT_ORANGE", "#E99A3A"),
-    LegoColor("TRANS_MEDIUM_BLUE", "#76A3C8"),
-    LegoColor("TRANS_NEON_GREEN", "#C0F500"),
-    LegoColor("TRANS_NEON_ORANGE", "#FF4231"),
-    LegoColor("TRANS_NEON_YELLOW", "#FFD700"),
-    LegoColor("TRANS_ORANGE", "#E96F01"),
-    LegoColor("TRANS_PURPLE", "#5525B7"),
-    LegoColor("TRANS_RED", "#9C0010"),
-    LegoColor("TRANS_YELLOW", "#EBF72D"),
-    LegoColor("CHROME_GOLD", "#F1F2E1"),
-    LegoColor("FLAT_DARK_GOLD", "#AD7118"),
-    LegoColor("FLAT_SILVER", "#8D949C"),
-    LegoColor("PEARL_GOLD", "#E79E1D"),
-    LegoColor("PEARL_LIGHT_GOLD", "#E7AE5A"),
-    LegoColor("PEARL_LIGHT_GRAY", "#ACB7C0"),
-    LegoColor("SATIN_TRANS_LIGHT_BLUE", "#68BCC5"),
-    LegoColor("METALLIC_GOLD", "#B8860B"),
-    LegoColor("METALLIC_SILVER", "#C0C0C0"),
-    LegoColor("MILKY_WHITE", "#D4D3DD"),
-    LegoColor("GLITTER_TRANS_LIGHT_BLUE", "#68BCC5"),
-]
